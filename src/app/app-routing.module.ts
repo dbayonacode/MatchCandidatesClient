211aa2ed3df1af276e514059a083cf3eb46d2c41
Diego <dbayonacode@gmail.com>
2019-04-22 17:35:54 +1000

    Creating scaffolding layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './candidate.matching/home/home.component';
+import { LayoutComponent } from './candidate.matching/layout/layout.component';
 
-const routes: Routes = [];
+const matchingCandidatesRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      } 
+    ]
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' } //wildcard routes
+  //TODO: Create ErrorComponent
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(matchingCandidatesRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './candidate.matching/layout/layout.component';
+import { HomeComponent } from './candidate.matching/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
